497cd7252c51eea4cda3cef3c020c4415c896252
pushpa1101 <pushpa@nuvepro.com>
2025-11-11 22:08:41 +0530

    Create app-start.sh

---
+++ app-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+export PATH="$PATH:/home/labuser/.local/bin"
+set -e
+ 
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
+FRONTEND_DIR="$BASE_DIR"
+ 
+echo "Starting EXLPlain application..."
+echo "Starting Frontend..."
+cd "$FRONTEND_DIR"
+streamlit run app.py &
+FRONTEND_PID=$!
+ 
+echo "Frontend PID: $FRONTEND_PID"
+trap "echo ' Stopping both...'; kill  $FRONTEND_PID" SIGINT
+wait
